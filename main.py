e450bc16924aaaae7ad137f65194f7dbbb860dd4
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-08 14:46:26 +0100

    Извлечение ссылок на картинки

--- main.py
+++ main.py
@@ -2,16 +2,41 @@ import requests
 from pathlib import Path
 
 
-folder_path = Path(r"D:\Py\SpacePhotos\images")
-folder_path.mkdir(parents=True, exist_ok=True)
+def get_links(launch_id):
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+    response = requests.get(url)
+    response.raise_for_status()
 
-picture_name = 'hubble.jpeg'
-picture_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    picture_urls = response.json().get('links', {}).get('flickr', {}).get('original', [])
 
-picture_path = folder_path / picture_name
+    if picture_urls:
+        print(picture_urls)
+    else:
+        print("Фотографии отсутствуют")
+    return picture_urls
 
-response = requests.get(picture_url)
-response.raise_for_status()
+launch_id = "5eb87d42ffd86e000604b384"
 
-with open(picture_path, 'wb') as file:
-    file.write(response.content)
+
+def download_image():
+    picture_url = input('Введите url картинки: ')
+    folder_path_input = input("Введите путь к папке для сохранения (например, D:/Images): ")
+    picture_name = input('Введите имя картинки: ')
+
+    folder_path = Path(folder_path_input)
+
+    folder_path.mkdir(parents=True, exist_ok=True)
+
+    picture_path = folder_path / picture_name
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    response = requests.get(picture_url, headers=headers)
+    response.raise_for_status()
+
+    with open(picture_path, 'wb') as file:
+        file.write(response.content)
+
+get_links(launch_id)
