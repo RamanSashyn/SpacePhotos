2757522e1b207c7eae6222067c7f0b68c8ed9643
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-09 17:20:07 +0100

    publish text to Telegram channel

---
+++ space_photos_bot.py
@@ -0,0 +1,5 @@
+import telegram
+
+
+bot = telegram.Bot(token='7565691055:AAFIXnusR6kmFPjIfnGwv_2czrRsBRYhupg')
+bot.send_message(text='Хочу вам представить канал с фотографиями космоса', chat_id='@spacephotodvmn')
