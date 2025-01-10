67ec86a7897b9db32ed718d1ecfaf4f92eda9862
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-10 15:50:46 +0100

    Добавление вспомогательная функция

---
+++ get_picture_from_directory.py
@@ -0,0 +1,11 @@
+import os
+
+
+def get_picture_from_directory(directory_path):
+    picture_extensions = ['jpg', 'jpeg', 'png', 'gif']
+    pictures = []
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if os.path.splitext(file)[1].lower() in [f".{ext}" for ext in picture_extensions]:
+                pictures.append(os.path.join(root, file))
+    return pictures
