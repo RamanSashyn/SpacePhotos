d15248d47d8cd60a0bd9d55b0875a435798a9598
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-10 15:50:57 +0100

    Добавление вспомогательная функция

---
+++ send_photo_to_telegram.py
@@ -0,0 +1,7 @@
+import telegram
+
+
+def send_photo_to_telegram(bot_token, chat_id, photo_path):
+    bot = telegram.Bot(token=bot_token)
+    with open(photo_path, 'rb') as photo:
+        bot.send_photo(chat_id=chat_id, photo=photo)
